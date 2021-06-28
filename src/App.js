2523d1e45d1f5d815b67ba10be74ba5507a7609b
bamfard@gmail.com <^,+jTk'(wiq"ybh&Ga5[y>
2021-06-28 14:17:25 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [firstDataRendered, setFirstDataRendered] = useState(false)
+    const rowData = [
+        {make: "Toyota", model: "Celica", price: 35000},
+        {make: "Ford", model: "Mondeo", price: 32000},
+        {make: "Porsche", model: "Boxter", price: 72000}
+    ];
+    const defaultColDef = {
+        flex: 1
+    }
+
+    const onFirstDataRendered = () => {
+        setFirstDataRendered(true)
+    }
+
+    return (
+        <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+            <p data-testid="dataRendered" style={{ position: 'absolute', left: -999 }}>{firstDataRendered ? "Data rendered to the grid" : null}</p>
+            <AgGridReact
+                rowData={rowData}
+                defaultColDef={defaultColDef}
+                onFirstDataRendered={onFirstDataRendered}
+            >
+                <AgGridColumn field="make"/>
+                <AgGridColumn field="model"/>
+                <AgGridColumn field="price" editable={true}/>
+            </AgGridReact>
+        </div>
+    );
+};
 
 export default App;
